aee3c37c65d0150dff859896856b3c579a30ccb2
Abdul Kalam Khan <subhankhan3786@gmail.com>
2025-09-16 01:58:45 +0530

    Fixed Day2_Swaping File

--- Day1/Day1_HelloWorld.java
+++ Day1/Day1_HelloWorld.java
@@ -1,5 +1,5 @@
-class Day1_HelloWorld{
-    public static void main(String[] args){
+public class Day1_HelloWorld {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
---
+++ Day2/Day2_Swapping.java
@@ -0,0 +1,12 @@
+public class Day2_Swapping {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        a = a + b; // 30
+        b = a - b; // 30 - 20 =  10
+        a = a - b; // 30 - 10 = 20
+
+        System.out.println("A: " + a + " | B: " + b);
+    }
+}
