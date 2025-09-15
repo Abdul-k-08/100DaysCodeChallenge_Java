a8e78b1702a8d0df46bd0308d874410232ef30a3
Abdul Kalam Khan <subhankhan3786@gmail.com>
2025-09-16 01:29:54 +0530

    My first commit from Intellij

---
+++ Day2/Day2_Variables.java
@@ -0,0 +1,10 @@
+class Day2_Variables{
+   public static void main(String[] args) {
+    int age = 25; //Stores in Stack and will contain 4 bytes
+    String name = "Abdul"; //Stores in Heap, Beacause String is an Object
+    double balance =  10552.55; // Stores in Stack and will contain 8 bytes
+
+    System.out.println("User: "+ name + " | Age: " + age + " | Balance: "+ balance);
+   }
+    
+}
