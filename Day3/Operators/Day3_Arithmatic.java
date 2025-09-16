d9d711bebfc8146c263b907afedd991e29576e20
Abdul Kalam Khan <subhankhan3786@gmail.com>
2025-09-16 12:14:09 +0530

    Operators

---
+++ Day3/Operators/Day3_Arithmatic.java
@@ -0,0 +1,18 @@
+package Day3.Operators;
+
+public class Day3_Arithmatic {
+    public static void main(String[] args) {
+        int a= 20;
+        int b= 7;
+
+        System.out.println("A ➕ B = " + (a+b)); //Addition
+
+        System.out.println("A ➖ B = " + (a-b)); // Subtraction
+
+        System.out.println("A ✖️ B = " + (a*b)); // Multiplication
+
+        System.out.println("A ➗ B = "+ ((double)a/b)); // Division
+
+        System.out.println("A % B = "+ (a%b) ); // Module --> Remainder
+    }
+}
---
+++ Day3/Operators/Day3_Assignment.java
@@ -0,0 +1,23 @@
+package Day3.Operators;
+
+public class Day3_Assignment {
+    public static void main(String[] args) {
+        int a  = 40; // Assign
+        System.out.println(a); //40
+
+        a += 20; //Add and Assign
+        System.out.println(a); //40 + 20 = 60
+
+        a-=10; //Subtract and Assign
+        System.out.println(a); //60 - 10 = 50
+
+        a*=2; //Multiply and Assign
+        System.out.println(a); //50 * 2 = 100
+
+        a/=2; //Divide and Assign
+        System.out.println(a); //100 / 2 = 50
+
+        a%=2; //Modulo and Assign
+        System.out.println(a); //50 % 2 = 0 --> 2*25=50, Remainder 0
+    }
+}
