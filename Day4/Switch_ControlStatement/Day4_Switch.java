95a4324ca67eff73522517a8743f7a15ca68ce53
Abdul Kalam Khan <subhankhan3786@gmail.com>
2025-09-19 23:04:20 +0530

    Learning Switch

---
+++ Day4/Switch_ControlStatement/Day4_Switch.java
@@ -0,0 +1,25 @@
+package Day4.Switch_ControlStatement;
+
+public class Day4_Switch {
+    public static void main(String[] args) {
+        int age = 4;
+
+        switch (age){
+            case 1:
+                System.out.println("Age is 1");
+                break;
+            case 2:
+                System.out.println("Age is 2");
+                break;
+            case 3:
+                System.out.println("Age is 3");
+                break;
+            case 4:
+                System.out.println("Age is 4");
+                break;
+            default:
+                System.out.println("Age is inifite");
+                break;
+        }
+    }
+}
